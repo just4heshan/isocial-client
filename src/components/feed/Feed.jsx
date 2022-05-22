e5f374391c5066db38e3eb4e02960a68236216e6
just4heshan <heshanravindussc@gmail.com>
2022-05-23 01:36:26 +1000

    Feed section completed

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import "./feed.css";    
 import SharePost from '../sharePost/SharePost';
+import Post from '../post/Post';
+import {Posts} from "../../dummyData"
 
 const Feed = () => {
   return (
@@ -9,8 +11,11 @@ const Feed = () => {
         <div className="feed-wrapper">
             <div className="feed-heading">
                 <div className="feed-top">
-                  <h3 className="heading-text">Feeds Here</h3>
+                  {/* <h3 className="heading-text">Feeds Here</h3> */}
                   <SharePost />
+                  {Posts.map((post) => (
+                    <Post key={post.id} post={post} />
+                  ))}
                 </div>
             </div>
         </div>
