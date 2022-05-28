79aaac5083afba65350c49f49b319060276f2594
just4heshan <heshanravindussc@gmail.com>
2022-05-28 23:12:44 +1000

    commited 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./index.css";
 
